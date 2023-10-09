39db8bf4d3c8123ca34d0d96795547f74223ff0d
Сергей Фуфаев <veafuf@ya.ru>
2023-10-09 09:50:00 +0300

    Deploy

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'django_insecure')
+SECRET_KEY = os.getenv('SECRET_KEY', 'django')
 
 DEBUG = bool(os.getenv('DEBUG', default='false') == 'true')
 
